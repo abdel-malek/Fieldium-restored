2e92ef571a3aaa6719bda0f3faf226c67e76e8ab
Amal Abdulraouf <amalabdulraouf@gmail.com>
2017-01-01 23:47:32 -0800

    remove website

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -7,7 +7,6 @@ class site extends REST_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model('Services/message_service');
         $this->load->model('Services/field_service');
         $this->load->model('Services/game_service');
     }
@@ -23,21 +22,6 @@ class site extends REST_Controller {
                 'games' => $games
                     )
             );
-        }
-    }
-
-    public function send_message_post() {
-        $this->load->helper('form');
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('name', 'Name', 'required|min_length[6]|max_length[30]');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-        $this->form_validation->set_rules('subject', 'Subject', 'required|min_length[6]|max_length[30]');
-        $this->form_validation->set_rules('text', 'Text', 'required|min_length[10]');
-        if (!$this->form_validation->run()) {
-            throw new Validation_Exception(validation_errors());
-        } else {
-            $this->message_service->save($this->post('name'), $this->post('email'), $this->post('subject'), $this->post('text'));
-            $this->response(array('status' => true, 'data' => null, 'message' => $this->lang->line('message_sent')));
         }
     }
 
