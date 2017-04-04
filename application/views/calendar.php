db18969faa1c6cbd32677d082b6b6d6027b6b74d
rabiides <r.desouki@nova-ue.com>
2017-04-04 14:09:28 +0200

    create cancel bookings
    create cancel bookings -ADDED-

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -1,19 +1,25 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fullcalendar/fullcalendar.min.css') ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/jquery-ui.min.css') ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/calendar.css') ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/incoming_booking_sideMenu.css') ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap-datepicker.css') ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/jquery.timepicker.css') ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/card.css') ?>" />
 <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/font-awesome.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fullcalendar/fullcalendar.print.css') ?>" media="print"/>
 <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/fullcalendar/moment.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/fullcalendar/fullcalendar.min.js') ?>"></script>
-<script type="text/javascript" src="<?php // echo base_url('assets/fullcalendar/fullcalendar-columns.js')      ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/fullcalendar/materialize.js') ?>"></script>
+<script type="text/javascript" src="<?php // echo base_url('assets/fullcalendar/fullcalendar-columns.js')        ?>"></script>
 <script type="text/javascript">
     var fields = '<?php echo json_encode($fields) ?>';
-    var approved = "<?php echo BOOKING_STATE::APPROVED?>";
+    var approved = "<?php echo BOOKING_STATE::APPROVED ?>";
 </script>
 <body> 
     <div class="container" style="">
         <?php $this->load->view('sideMenu') ?>
+        <?php $this->load->view('incoming_booking') ?>
         <div class="main-content">
             <div class="swipe-area"></div>
             <a href="#" data-toggle=".container" id="sidebar-toggle">
@@ -92,10 +98,153 @@
                         </div>
                         <br>
                         <div class="modal-footer">
-                            <button class="btn btn-warning" data-dismiss="modal">Close</button>
+                            <div class="col-md-6 left">
+                                <button class="btn btn-primary close-btn" onclick="show_booking_model()">Cancel</button>
+                            </div>
+                            <div class="col-md-6 right">
+                                <button class="btn btn-warning" data-dismiss="modal">Close</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            </body>
-            <script type="text/javascript" src="<?php echo base_url('assets/js/calendar.js') ?>"></script>
+        </div>
+
+        <div  class="modal" id="new_booking_modal" >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 style="display: inline;">New Booking</h3>
+                    </div>
+                    <div class="modal-body" >
+                        <div class="row margin-top-5px">
+                            <div class="col-md-3">
+                                <b>Player Name: </b>
+                            </div>
+                            <div class="col-md-8">
+                                <input id="player_name" type="text" class="form-control" placeholder="Player Name" aria-describedby="sizing-addon1">
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px">
+                            <div class="col-md-3">
+                                <b>Player Phone: </b>
+                            </div>
+                            <div class="col-md-8">
+                                <input id="player_phone" type="text" class="form-control" placeholder="Player Phone`" aria-describedby="sizing-addon1">
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px"> 
+                            <div class="col-md-3">
+                                <b>Field: </b>
+                            </div>
+                            <div class="col-md-8">
+                                <select class="form-control" id="fields-list" onchange="get_current_field_games();">
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px"> 
+                            <div class="col-md-3">
+                                <b>Game: </b>
+                            </div>
+                            <div class="col-md-8">
+                                <select class="form-control" id="games-list" onchange="init_minutes_select();">
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px">
+                            <div class="col-md-3">
+                                <b>Date: </b>
+                            </div>
+                            <div class="col-md-8" id="booking_date_div">
+                                <label id="booking_date">2017-7-7</label>
+                                <input type="hidden" id="booking_datepicker" onclick="open_booking_datepicker()">
+                                </input>
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px">
+                            <div class="col-md-3">
+                                <b>Start Time: </b>
+                            </div>
+                            <div class="col-md-8 right" id="timepicker_div">
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px">
+                            <div class="col-md-3">
+                                <b>Duration: </b>
+                            </div>
+                            <div class="col-md-8 right">
+                                <div class="col-md-6">
+                                    <label>Hours</label>
+                                    <select  class="time-list" id="hours-list" onchange="update_total();">
+                                    </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Minutes</label>
+                                    <select  class="time-list" id="minutes-list" onchange="update_total();">
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px">
+                            <div class="col-md-3">
+                                <b>Note: </b>
+                            </div>
+                            <div class="col-md-8">
+                                <input id="note" type="text" class="form-control" placeholder="Note" aria-describedby="sizing-addon1">
+                            </div>
+                        </div>
+                        <div class="row margin-top-5px">
+                            <div class="col-md-3">
+                                <b>Total: </b>
+                            </div>
+                            <div class="col-md-8">
+                                <p id="book_total_label"></p>
+                            </div>
+                            <div class="col-md-8">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="modal-footer">
+                            <div class="col-md-6 left">
+                                <button class="btn btn-primary create-btn" onclick="booking_create();">Create</button>
+                            </div>
+                            <div class="col-md-6 right">
+                                <button class="btn btn-warning" id="close">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div  class="modal" id="cancel_booking_modal" style="margin-top: 10%;" >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 style="display: inline;">Select a message to send to the client</h3>
+                    </div>
+                    <div class="modal-body" >
+                        <div class="row margin-top-5px center">
+                            <select id="refuse_msgs-list">
+                            </select>
+                        </div>
+                        <div class="row margin-top-5px center">
+                            <input id="new-cancel-msg" type="text" class="" placeholder="new refuse message" aria-describedby="sizing-addon1">
+                        </div>
+                        <br>
+                        <div class="modal-footer">
+                            <div class="col-md-6 left">
+                                <button class="btn btn-danger create-btn" onclick="booking_cancel();">Cancel</button>
+                            </div>
+                            <div class="col-md-6 right">
+                                <button class="btn btn-warning" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.timepicker.js') ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/calendar.js') ?>"></script>
+</body>
+
--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -16,6 +16,29 @@ for (var i = 0; i < fields.length; i++) {
     j++;
 }
 $.ajax({
+    url: site_url + '/bookings/get_cancellation_reasons/format/json',
+    type: 'GET',
+    async: false,
+    success: function (response) {
+        if (response.status == true) {
+            var msgs = response.data;
+            $select = $('#refuse_msgs-list');
+            $select.html("");
+            $select.append($("<option></option>")
+                                    .attr("value", "msgs[i].message")
+                                    .attr("id", -1)
+                                    .text(""));
+            for (var i = 0; i < msgs.length; i++) {
+                $select.append($("<option></option>")
+                                    .attr("value", msgs[i].message)
+                                    .attr("id", msgs[i].id)
+                                    .text(msgs[i].message));
+            }
+        } else
+            alert("Error in loading bookings");
+    }
+});
+$.ajax({
     url: site_url + '/bookings/company_bookings/format/json',
     type: 'GET',
     async: false,
@@ -53,7 +76,6 @@ $.ajax({
             alert("Error in loading bookings");
     }
 });
-console.log(events);
 var calendar = $('#calendar').fullCalendar({
     header: {
         right: 'title',
@@ -72,6 +94,7 @@ var calendar = $('#calendar').fullCalendar({
     allDaySlot: false,
     eventClick: function (calEvent, jsEvent, view) {
         $('#booking_num').html("Booking #" + calEvent.id);
+        $('#booking_num').attr("book_id",calEvent.id);
         $('#player_name').html(calEvent.player_name);
         $('#player_phone').html(calEvent.player_phone);
         $('#start_label').html(moment(calEvent.start_time, "HH:mm:ss").format("HH:mm A"));
@@ -83,6 +106,31 @@ var calendar = $('#calendar').fullCalendar({
         $('#note_label').html(calEvent.note);
         $('#field_label').html(calEvent.company_name + " - " + calEvent.field_name);
         $('#booking_modal').modal("show");
+
+    },
+    
+    dayClick: function(date, jsEvent, view) {
+
+        //alert('Clicked on: ' + date.format());
+          get_all_fields();
+          init_hours_select();
+          init_minutes_select();
+          $('#timepicker_div').html('<input class="timepicker " >');
+          
+          $('.timepicker').timepicker({
+            timeFormat: 'H:mm',
+            interval: 60,
+            minTime: date.format("H:mm"),
+            maxTime: '11:00pm',
+            defaultTime: date.format("H:mm"),
+            startTime: date.format("H:mm"),
+            dynamic: false,
+            dropdown: true,
+            scrollbar: true,
+        });
+          
+         $('#booking_date').text($.datepicker.formatDate('yy-m-d', new Date(date)));
+        $('#new_booking_modal').modal("show");
 
     }
 });
@@ -101,3 +149,224 @@ $('#datepicker').datepicker({
 function open_datepicker() {
     $('#datepicker').focus().hide();
 }
+$.ajax({
+    url: site_url + '/bookings/company_bookings/format/json',
+    type: 'GET',
+    async: false,
+    success: function (response) {
+        if (response.status == true) {
+            var booking;
+            bookings = response.data;
+            for (var i = 0; i < bookings.length; i++) {
+//                if (bookings[i].state_id == approved) {
+                    start = new Date(bookings[i].start);
+                    end = moment(bookings[i].start, "HH:mm:ss").add(parseInt(bookings[i].duration), "minutes").format("HH:mm:ss");
+                    booking = {
+                        id: bookings[i].booking_id,
+                        start: bookings[i].date + "T" + bookings[i].start,
+                        end: bookings[i].date + "T" + end,
+                        color: fieldscolors[bookings[i].field_id],
+                        resources: [bookings[i].field_id],
+                        player_name: bookings[i].player_name,
+                        title: "#" + bookings[i].booking_id + " " + bookings[i].player_name,
+                        player_phone: bookings[i].player_phone,
+                        field_name: bookings[i].field_name,
+                        company_name: bookings[i].company_name,
+                        game_name: bookings[i].game_type_name,
+                        hour_rate: bookings[i].hour_rate,
+                        total: bookings[i].total,
+                        duration: bookings[i].duration,
+                        start_time: bookings[i].start,
+                        date: bookings[i].date,
+                        note: bookings[i].note,
+                    };
+                    events.push(booking);
+//                }
+            }
+        } else
+            alert("Error in loading bookings");
+    }
+});
+function get_all_fields() {
+    $select = $('#fields-list');
+    $select.html("");
+    for (var i = 0; i < fields.length; i++) {
+    $select.append($("<option></option>")
+                        .attr("value", fields[i].name)
+                        .attr("id", fields[i].field_id)
+                        .attr("index",i)
+                        .text(fields[i].name));
+    }
+    get_current_field_games();
+}
+
+function get_current_field_games() {
+    var index= $('#fields-list').find(":selected").attr("index");
+    $select = $('#games-list');
+    $select.html("");
+    for (var i = 0; i < fields[index].games.length; i++) {
+    $select.append($("<option></option>")
+                        .attr("value", fields[index].games[i].name)
+                        .attr("id", fields[index].games[i].game_type_id)
+                        .attr("index",i)
+                        .text(fields[index].games[i].name));
+    }
+    init_minutes_select();
+}
+
+function init_hours_select(){
+    $select = $('#hours-list');
+    $select.html("");
+    for (var i = 0; i < 24; i++) {
+    $select.append($("<option></option>")
+                        .attr("value", i)
+                        .text(i));
+    }
+}
+
+function init_minutes_select(){
+    var f_index= $('#fields-list').find(":selected").attr("index");
+    var g_index= $('#games-list').find(":selected").attr("index");
+    $select = $('#minutes-list');
+    $select.html("");
+    for (var i = 0; fields[f_index].games[g_index].increament_factor*i<60; i++) {
+    $select.append($("<option></option>")
+                        .attr("value", fields[f_index].games[g_index].increament_factor*i)
+                        .text(fields[f_index].games[g_index].increament_factor*i));
+    }
+    update_total()
+}
+
+function update_total(){
+    var f_index= $('#fields-list').find(":selected").attr("index");
+    var total=fields[f_index].hour_rate/(60/$('#minutes-list').find(":selected").attr("value"));
+    total+=fields[f_index].hour_rate*$('#hours-list').find(":selected").attr("value");
+    $('#book_total_label').html(total + " AED");
+}
+
+
+$('#booking_datepicker').datepicker({
+    buttonImage: base_url + 'assets/images/calendar.png',
+    buttonImageOnly: true,
+//    changeMonth: true,
+//    changeYear: true,
+    showOn: 'both',
+    onSelect: function (value) {
+         $('#booking_date').text($.datepicker.formatDate('yy-m-d', new Date(value)));
+    }
+});
+function open_booking_datepicker() {
+    $('#booking_datepicker').focus().hide();
+}
+
+
+
+function booking_create(){
+    var Data = {};
+    
+    Data['field_id']=$('#fields-list').find(":selected").attr("id");
+    Data['game_type'] =$('#games-list').find(":selected").attr("id");
+    Data['player_phone'] =$('#new_booking_modal #player_phone').val();
+    Data['player_name'] =$('#new_booking_modal #player_name').val();
+    Data['start'] =$('.timepicker').val()+":00";
+    Data['duration'] = parseFloat($('#hours-list').find(":selected").attr("value")*60)+parseFloat($('#minutes-list').find(":selected").attr("value"));
+    Data['date'] =$('#new_booking_modal #booking_date').html();
+    Data['notes'] =$('#new_booking_modal #note').val();
+    
+    $.ajax({
+    url: site_url + '/bookings/create_manually/format/json',
+    type: 'POST',
+    data:Data,
+    async: false,
+    success: function (response) {
+        if (response.status == true) {
+            close();
+            var booking = response.data;
+            start = new Date(booking.start);
+            end = moment(booking.start, "HH:mm:ss").add(parseInt(booking.duration), "minutes").format("HH:mm:ss");
+            booking = {
+                id: booking.booking_id,
+                start: booking.date + "T" + booking.start,
+                end: booking.date + "T" + end,
+                color: fieldscolors[booking.field_id],
+                resources: [booking.field_id],
+                player_name: booking.player_name,
+                title: "#" + booking.booking_id + " " + booking.player_name,
+                player_phone: booking.player_phone,
+                field_name: booking.field_name,
+                company_name: booking.company_name,
+                game_name: booking.game_type_name,
+                hour_rate: booking.hour_rate,
+                total: booking.total,
+                duration: booking.duration,
+                start_time: booking.start,
+                date: booking.date,
+                note: booking.note,
+            };
+            events.push(booking);
+            $('#calendar').fullCalendar( 'renderEvent', booking );
+        } else
+            alert(response.message);
+    }
+    });
+}
+
+function show_booking_model(){
+    $('#cancel_booking_modal').modal("show");
+}
+
+function booking_cancel(){
+    var booking_id=$('#booking_num').attr('book_id');
+    var reason_id=$('#refuse_msgs-list').find(":selected").attr("id");
+    var note=$('#new-cancel-msg').val();
+    var data="";
+     if(reason_id==-1&&note==''){
+         alert('Please select a message or write one!');
+         return;
+     }
+    if(reason_id!=-1){
+        data='&reason_id='+reason_id;
+    }
+    if(note!=""){
+        data=data+'&note='+note;
+    }
+    $.ajax({
+    url: site_url + '/bookings/cancel/format/json?booking_id=' + booking_id+data,
+    type: 'GET',
+    async: false,
+    success: function (response) {
+        if (response.status == true) {
+            $('#cancel_booking_modal').modal("hide");
+            $('#booking_modal').modal("hide");
+            $('#calendar').fullCalendar( 'removeEvents',booking_id);
+        } else
+            alert(response.message);
+    }
+    });
+}
+
+$( "#close" ).click(function() {
+    close();
+});
+
+function close(){
+    $('#fields-list').html('');
+    $('#games-list').html('');
+    $('#new_booking_modal #player_phone').val('');
+    $('#new_booking_modal #player_name').val('');
+    $('#new_booking_modal #booking_date').html('');
+    $('#new_booking_modal #note').val('');
+    $('#book_total_label').html("");
+    $('#new_booking_modal').modal("hide");
+}
+$( "#booking_date_div" ).click(function() {
+    
+});
+function openNav() {
+                document.getElementById("incoming_booking_Sidenav").style.width = "400px";
+            }
+
+            /* Set the width of the side navigation to 0 */
+            function closeNav() {
+                document.getElementById("incoming_booking_Sidenav").style.width = "0";
+            }
