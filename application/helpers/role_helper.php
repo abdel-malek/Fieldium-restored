1dd40d9d69ce400bab6466484d35bc9e2de41990
AmalAbdulraouf <amalabdulraouf@gmail.com>
2017-08-20 11:31:36 +0300

    iOS App

--- application/helpers/role_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class ROLE {
-
-    const ADMIN = 1;
-    const SUPPORT = 2;
-    const CASHIER = 3;
-}
--- application/language/english/views_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang['field_not_found'] = 'Field is not found ';
-$lang['offer_not_found'] = 'Offer is not found ';
-$lang['companies_management'] = "Companies Management";
-$lang['It is not a valid voucher'] = "Oops, it is not a valid voucher.";
-$lang['valid_voucher'] = "It is a valid voucher.";
-$lang['Country type is not found'] = "Game type is not found";
